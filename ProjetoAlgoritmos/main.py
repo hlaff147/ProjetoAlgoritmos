64c8287762ff1539263bfaa80c4edb38e1babb09
Sérgio Barreto <70080558+sergiolbarreto@users.noreply.github.com>
2021-04-08 12:09:12 -0300

    adicionando a interface gráfica

--- ProjetoAlgoritmos/main.py
+++ ProjetoAlgoritmos/main.py
@@ -1,33 +1,25 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import Dijkstra as dj
 
 
-data_distance = pd.read_csv (r'C:\Users\betin\OneDrive\Área de Trabalho\ProjetoAlgoritmos\CountyDistances.csv')
-data_names = pd.read_csv (r'C:\Users\betin\OneDrive\Área de Trabalho\ProjetoAlgoritmos\CountyNames.csv')
+data_distance = pd.read_csv (r'C:\Users\Wagner Bernardo\Creative Cloud Files\everything\meus programinhas\algoritmo\projetoalg\ProjetoAlgoritmos\ProjetoAlgoritmos\CountyDistances.csv', encoding='latin-1')
+data_names = pd.read_csv (r'C:\Users\Wagner Bernardo\Creative Cloud Files\everything\meus programinhas\algoritmo\projetoalg\ProjetoAlgoritmos\ProjetoAlgoritmos\CountyNames.csv', encoding='latin-1')
 df = pd.DataFrame(data_distance, columns= ['county1','mi_to_county','county2'])
 
 dicg = {}
-c1 = df['county1'].unique().tolist()
-distance = df['mi_to_county'].unique().tolist()
-c2 = df['county2'].unique().tolist()
+c1 = df['county1'].tolist()
+distance = df['mi_to_county'].tolist()
+c2 = df['county2'].tolist()
+
+for i in range(0, len(c1)-1):
+    dicg[int(c1[i])] = {}
+for i in range(0,len(c1)-1):
+    dicg[int(c1[i])][int(c2[i])] = distance[i]
 
 
 
-for i in c1:
-    dicg[i] = {}
-    a = df.query(f'county1 == {i}')
-
-    for ind, j in a.iterrows():
-        dicg[i][int(j['county2'])] = j['mi_to_county']
-
-
-# print(dicg)
-
-dj.dijkstra_path(dicg, 1001, 17095)
-
-   
-    
-
-
+print(data_names)
+print("Escolha duas cidades(informando seu respectivo código) e saiba qual menor trajeto e sua distancia.")
+cid1 = int(input("Qual a primeira cidade?"))
+cid2 = int(input("Qual a segunda cidade?"))
+dj.dijkstra_path(dicg, cid1, cid2)
