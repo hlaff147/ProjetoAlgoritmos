0604a0b186da490d6bd0c0dfb342ecc34c5d880d
Sérgio Barreto <70080558+sergiolbarreto@users.noreply.github.com>
2021-04-08 16:41:10 -0300

    atualizar a interface

--- ProjetoAlgoritmos/graphic_interface.py
+++ ProjetoAlgoritmos/graphic_interface.py
@@ -5,9 +5,12 @@ import matplotlib.pyplot as plt
 import Dijkstra as dj
 
 st.title('County Distance')
-st.subheader('\"If you don\'t like the road you\'re walking, start paving another one.\"')
-st.subheader('- Dolly Parton')
-st.header('    Abaixo segue uma parte de um banco de dados de distâncias de um condado ao outro. Os condados são dos arquivos Census 2000 SF1 e Census 2010 SF1.  Os códigos de condado são códigos de condado FIPS. Os primeiros dois dígitos dos códigos de condado FIPS são códigos de estado FIPS. Abaixo você pode digitar dois condados e vê a menos distância entre eles.')
+st.write('')
+st.markdown(""" ** \"If you don\'t like the road you\'re walking, start paving another one.\"  
+\- Dolly Parton ** """, True)
+st.write('')
+st.write('Abaixo segue um banco de dados de nomes de condados. Os condados são dos arquivos Census 2000 SF1 e Census 2010 SF1.  Os códigos de condado são códigos de condado FIPS. Os primeiros dois dígitos dos códigos de condado FIPS são códigos de estado FIPS. Abaixo você pode digitar dois condados, pelo seu código (que se encontra ao lado ) e ver a menor distância entre eles.')
+st.write('Fonte do banco de dados: https://www.nber.org/research/data/county-distance-database')
 
 
 @st.cache
@@ -35,9 +38,9 @@ for i in range(0,len(c1)-1):
 
 
 st.write(data)
-st.write('Escolha duas cidades(informando seu respectivo código) e saiba qual menor trajeto e sua distancia.')
-user_input = st.text_area("Qual a primeira cidade?")
-user_input1 = st.text_area("Qual a segunda cidade?")
+st.write('Escolha dois condados(informando seu respectivo código) e saiba qual menor trajeto e sua distancia.')
+user_input = st.text_area("Qual o primeiro condado?")
+user_input1 = st.text_area("Qual o segundo condado?")
 
 if st.button('Enviar'):
     result = int(user_input)
