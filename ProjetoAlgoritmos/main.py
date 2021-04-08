85603467f3198889f01db04caca4d730f74b592b
HUMBERTO LIMA DE ALCANTARA FONSECA FILHO <hlaff@cin.ufpe.br>
2021-04-08 11:04:04 -0300

    Update main.py

--- ProjetoAlgoritmos/main.py
+++ ProjetoAlgoritmos/main.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import Dijkstra as dj
 
 
@@ -13,8 +11,6 @@ c1 = df['county1'].unique().tolist()
 distance = df['mi_to_county'].unique().tolist()
 c2 = df['county2'].unique().tolist()
 
-
-
 for i in c1:
     dicg[i] = {}
     a = df.query(f'county1 == {i}')
@@ -22,12 +18,8 @@ for i in c1:
     for ind, j in a.iterrows():
         dicg[i][int(j['county2'])] = j['mi_to_county']
 
-
-# print(dicg)
-
-dj.dijkstra_path(dicg, 1001, 17095)
-
-   
-    
-
-
+print(data_names)
+print("Escolha duas cidades(informando seu respectivo código) e saiba qual menor trajeto e sua distancia.")
+cid1 = int(input("Qual a primeira cidade?"))
+cid2 = int(input("Qual a segunda cidade?"))
+dj.dijkstra_path(dicg, cid1, cid2)
